1cb196aa999bd2722a99e7566876b3f11febdd12
Alisson Perim Honostorio <alissonhonostorio@gmail.com>
2021-06-15 14:59:24 -0300

    Finalizado exercício 13 e iniciado exercício 15

--- Exercício 13/exercicio13.js
+++ Exercício 13/exercicio13.js
@@ -2,4 +2,44 @@
 //de 100, 50, 10, 5 e 1 em que o valor lido pode ser decomposto. Escrever o valor lido e a relação
 //de notas necessárias.
 
-console.log(130/100)
+
+function bestChange(x){
+    let nota100 = 0
+    let nota50 = 0
+    let nota10 = 0
+    let nota5 = 0
+    let nota1 = 0
+    
+    console.log(`Você possui: ${x.toLocaleString("pt-BR", {style: "currency", currency: "BRL"})}`)
+    
+    while((x / 100) >= 1){
+        x -= 100
+        nota100 += 1
+    }
+
+    while((x / 50) >= 1){
+        x -= 50
+        nota50 += 1
+    }
+
+    while((x / 10) >= 1){
+        x -= 10
+        nota10 += 1
+    }
+
+    while((x / 5) >= 1){
+        x -= 5
+        nota5 += 1
+    }
+
+    while((x / 1) >= 1){
+        x -= 1
+        nota1 += 1
+    }  
+
+    console.log(`Para chegar a esse valor você precisa de: ${nota100} nota(s) de 100 reais, ${nota50}
+                nota(s) de 50 reais, ${nota10} nota(s) de 10 reais, ${nota5} nota(s) de 5 reais e ${nota1} nota(s) de 1 real.`)
+}
+
+
+bestChange(329)
