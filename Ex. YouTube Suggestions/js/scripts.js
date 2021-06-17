2a60d0c3d8007f0f6b59e644843811a976095893
Alisson Perim Honostorio <alissonhonostorio@gmail.com>
2021-06-17 16:20:31 -0300

    Finalizado projeto Youtube suggestions

--- Ex. YouTube Suggestions/js/scripts.js
+++ Ex. YouTube Suggestions/js/scripts.js
@@ -1,26 +1,78 @@
-let textArea = document.querySelector('#textarea')
-let sugestionBlock = document.querySelector('.suggestion-block')
+let textArea = document.querySelector('#txtarea')
+let container = document.querySelector('.container')
+let sugestionContainer = document.querySelector('.suggestion-container')
+let suggestionsCount = document.querySelector('.suggestions-count--strong')
+let suggestionsBlock = document.querySelectorAll('.suggestion-block')
+let btnVote = document.querySelectorAll('.btn-count')
+
+
+let newSuggestion
+let allSuggestions = []
 
 const textAreaEnter = () => {
+    let count = 1
     textArea.addEventListener('keydown', (e) => {
-        console.log(e.key)
         if(e.key === 'Enter'){
-            sugestionBlock += `
-            <div class="vote-container">
-            <button class="btn-count">
-                <i class="fas fa-chevron-up"></i>
-            </button>
-            <p class="count-votes">
-                <span class="count-numbers">0</span>
-                votes
-            </p>
-            </div>
-            <div class="suggestions">
-                <strong class="suggestions--strong">${e.value}</strong>
+            let txtAreaValue = document.querySelector("#txtarea").value
+            e.preventDefault()
+            newSuggestion = `
+            <div id="suggestion-block" class="suggestion-block">
+                <div class="vote-container">
+                    <button class="btn-count">
+                        <i class="fas fa-chevron-up"></i>
+                    </button>
+                    <p class="count-votes">
+                        <span class="count-numbers">0</span>
+                        votes
+                    </p>
+                </div>
+                <div class="suggestions">
+                    <strong class="suggestions--strong">${txtAreaValue}</strong>
+                </div>
             </div>
             `
-        }   
+            allSuggestions = [...allSuggestions, newSuggestion]
+
+            count++
+            suggestionsCount.innerText = count
+            sugestionContainer.innerHTML += newSuggestion
+            document.querySelector("#txtarea").value = ""
+            suggestionsBlock.length++
+        }
     })
 }
 
-textAreaEnter()
+const setVote = () => {
+    container.addEventListener('click', (e) => {
+        const btn = e.path.find(el => el.classList && el.classList.contains('btn-count'))
+        console.log("listener container")
+        if(btn){
+            const block = e.path.find(el => el.classList && el.classList.contains('suggestion-block'))
+            console.log("event btn")
+            if(btn.classList.contains('voted')){
+                btn.classList.remove('voted')
+                unvote(block)
+            }else{
+                btn.classList.add('voted')
+                vote(block)
+            }
+        }
+    })
+}
+
+const vote = (el) => {
+    let vBtn = el.querySelector('.count-numbers')
+    vBtn.innerText = +vBtn.innerText + 1
+}
+
+const unvote = (el) => {
+    let vBtn = el.querySelector('.count-numbers')
+    vBtn.innerText = +vBtn.innerText - 1
+}
+
+const init = () => {
+    textAreaEnter()
+    setVote()
+}
+
+init()
